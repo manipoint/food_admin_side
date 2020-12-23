bb39be1c3825980ea3838288bdc83cd968921986
manipoint <manipoint@gmail.com>
2020-12-23 13:45:29 +0500

    image

--- lib/Screen/AddProduct/add_product_web.dart
+++ lib/Screen/AddProduct/add_product_web.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_image_picker/flutter_web_image_picker.dart';
 
 class AddProductWeb extends StatefulWidget {
   @override
@@ -6,10 +7,32 @@ class AddProductWeb extends StatefulWidget {
 }
 
 class _AddProductWebState extends State<AddProductWeb> {
+  Image image;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("Add Product Web"),),
+    return Scaffold(
+      body: Center(
+          child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width / 2,
+            height: MediaQuery.of(context).size.height / 2,
+            child: OutlineButton(
+              onPressed: () async {
+                final _image = await FlutterWebImagePicker.getImage;
+                setState(() {
+                  image = _image;
+                });
+              },
+              child: image != null
+                  ? image
+                  : Icon(Icons.add_a_photo_outlined),
+            ),
+          ),
+        ],
+      )),
     );
   }
+
+  
 }
